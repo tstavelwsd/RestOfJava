c47c38b02b5332b08eef60e4d5caa3f23358af13
tstave <tstave@484L083362.lwsd.k12wa.org>
2022-05-13 14:47:44 -0700

    Added a functional interface

---
+++ FuncPlotter/src/GetY.java
@@ -0,0 +1,5 @@
+
+public interface GetY {
+	//takes and x and calculates the y value
+	int calcY(int x);
+}
--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -3,26 +3,31 @@ import java.awt.Color;
 import graphics.Canvas;
 
 public class Program {
-    // Canvas GUI instance providing the drawing area on which to plot the functions
-    private static Canvas canvas;
+	// Canvas GUI instance providing the drawing area on which to plot the functions
+	private static Canvas canvas;
 
-    public static void main(String[] args) {
-        
-        // Create the canvas instance, set its range to x:[-360, 360] and y:[-240, 240] then 
-        // open it on the screen. Use canvas.plot(pX, pY) to plot a point at the (pX, pY) coords
-        canvas = new Canvas();
-        canvas.setRange(-360, -240, 360, 240);
-        canvas.open();
+	public static void main(String[] args) {
 
-        // Draw a short red diagonal on the canvas
-        canvas.pause();
-        canvas.setColor(Color.red);
-        for (int i = 0; i < 100; i++) {
-            canvas.plot(i, i);
-        }
+		// Create the canvas instance, set its range to x:[-360, 360] and y:[-240, 240]
+		// then
+		// open it on the screen. Use canvas.plot(pX, pY) to plot a point at the (pX,
+		// pY) coords
+		canvas = new Canvas();
+		canvas.setRange(-360, -240, 360, 240);
+		canvas.open();
 
-        // Pause and close the canvas then terminate the program.
-        canvas.pause();
-        canvas.close();
-    }
+		// Draw a short red diagonal on the canvas
+		canvas.pause();
+		canvas.setColor(Color.red);
+		GetY y = (x) -> {
+			return (int) (200 * Math.sin(Math.PI * x / 360));
+		};
+		for (int x = -360; x < 360; x++) {
+			canvas.plot(x, y.calcY(x));
+		}
+
+		// Pause and close the canvas then terminate the program.
+		canvas.pause();
+		canvas.close();
+	}
 }
