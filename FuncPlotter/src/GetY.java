0ecc46e84c012f4a5b45f33deb48b92b17462f2a
tstave <tstave@484L083362.lwsd.k12wa.org>
2022-05-17 14:48:36 -0700

    Showed Varargs and Anonymous Classes

--- FuncPlotter/src/GetY.java
+++ FuncPlotter/src/GetY.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 
 public interface GetY {
 	//takes and x and calculates the y value
 	int calcY(int x);
+	Color getColor();
 }
--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -19,15 +19,74 @@ public class Program {
 		// Draw a short red diagonal on the canvas
 		canvas.pause();
 		canvas.setColor(Color.red);
-		GetY y = (x) -> {
-			return (int) (200 * Math.sin(Math.PI * x / 360));
+		GetY sin = new GetY() {
+			public int calcY(int x) {
+				return (int) (200 * Math.sin(Math.PI * x / 360));
+			}
+			public Color getColor() {
+				return Color.blue;
+			}		
 		};
-		for (int x = -360; x < 360; x++) {
-			canvas.plot(x, y.calcY(x));
-		}
-
+		
+		GetY quadratic = new GetY() {
+			public int calcY(int x) {
+				return (int) ( (x - 200) * (x + 200) / 250  ); 
+			}
+			public Color getColor() {
+				return Color.orange;
+			}		
+		};
+		
+		
+		GetY log = new GetY() {
+			public int calcY(int x) {
+				return (int) ( 20 * Math.log(x)  ); 
+			}
+			public Color getColor() {
+				return Color.magenta;
+			}		
+		};
+	
+		
+		GetY cubic = new GetY() {
+			public int calcY(int x) {
+				return (int) ( Math.pow(x,3) / 200000  ); 
+			}
+			public Color getColor() {
+				return Color.green;
+			}		
+		};
+		
+		
+		GetY[] functions = {sin, quadratic, log, cubic};
+		
+		//plotFunction(sin);
+		//plotFunction(quadratic);
+		//plotFunction(log);
+		//plotFunction(cubic);
+		//plotFunction(functions);
+		plotFunction(sin, quadratic, log, cubic);
+		//plotFunction(sin, quadratic, cubic);
+		//plotFunction(functions);
+		//plotFunction();
+			
 		// Pause and close the canvas then terminate the program.
 		canvas.pause();
 		canvas.close();
 	}
+	
+	/** Plots a given function
+	 * 
+	 * @param function the function to plot
+	 */
+	public static void plotFunction(GetY... functions) {
+		for(int i = 0; i < functions.length; i++) {
+			canvas.setColor(functions[i].getColor());
+			for (int x = -360; x < 360; x++) {
+				canvas.plot(x, functions[i].calcY(x));
+			}
+		}
+	}
+	
+	
 }
