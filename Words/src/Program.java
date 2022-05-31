a8095002110fc9d3ba6e3ce68ebf69c3910cdd31
Florin Teodorescu <Florin.Teo@live.com>
2022-05-31 15:58:12 -0700

    Add MobyDick

--- Words/src/Program.java
+++ Words/src/Program.java
@@ -1,4 +1,9 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Program {
     
@@ -6,53 +11,59 @@ public class Program {
         System.out.println();
         System.out.println("Available commands:");
         System.out.println("?|help:\n  Prints this help message.");
-        System.out.println("add:\n  Add a word definition to the database.");
-        System.out.println("list:\n  List all words in the database.");
-        System.out.println("search:\n  Search the definition of a given word.");
+        System.out.println("load {file_name}:\n  Loads the words from the give file.");
+        System.out.println("most {N}:\n  List the most N frequent words.");
+        System.out.println("least {N}:\n  List the least N frequent words.");
+        System.out.println("search {word}:\n  Prints the frequency of the given word.");
         System.out.println("quit|exit:\n  Quit the program.");
         System.out.println();
     }
     
-    private static void processAdd(Scanner console) {
-        System.out.print("Enter word:> ");
-        String word = console.nextLine();
-        System.out.print("Enter definition:> ");
-        String definition = console.nextLine();
-        word = word.replaceAll(",|:|;|\\.", "");
-        System.out.println(word);
-    }
-
     public static void main(String[] args) {
         boolean done = false;
         System.out.println("Welcome to the world of WORDS!");
         Scanner console = new Scanner(System.in);
+        TextProcessor textProcessor = new TextProcessor();
         
         do {
             System.out.print("Command (? for help)> ");
-            String command = console.nextLine();
-            switch(command.toLowerCase()) {
-            case "?":
-            case "help":
-                processHelp();
-                break;
-            case "add":
-                processAdd(console);
-                break;
-            case "list":
-                break;
-            case "search":
-                break;
-            case "quit":
-            case "exit":
-                done = true;
-                break;
-            case "":
-                break;
-            default:
-                System.out.printf("Command '%s' is not supported.\n", command);
+            String[] line = console.nextLine().split(" ");
+            String command = line[0];
+            String arg = line.length > 1 ? line[1] : "";
+            
+            try {
+                switch(command.toLowerCase()) {
+                case "?":
+                case "help":
+                    processHelp();
+                    break;
+                case "load":
+                    textProcessor.processLoad(arg);
+                    break;
+                case "most":
+                    textProcessor.processMost(Integer.parseInt(arg));
+                    break;
+                case "least":
+                    textProcessor.processLeast(Integer.parseInt(arg));
+                    break;
+                case "search":
+                    textProcessor.processSearch(arg);
+                    break;
+                case "quit":
+                case "exit":
+                    done = true;
+                    break;
+                case "":
+                    break;
+                default:
+                    throw new Exception("Command not supported.");
+                }
+            } catch (Exception e) {
+                System.out.println("##ERROR##: " + e.getMessage());
             }
         } while(!done);
         
+        console.close();
         System.out.println("Goodbye!");
     }
 
---
+++ Words/src/TextProcessor.java
@@ -0,0 +1,59 @@
+import java.io.File;
+
+/**
+ * Class loading text from a file and extracting specific
+ * statistics such as words frequency.
+ */
+public class TextProcessor {
+    
+    /* TODO: Define a HashMap mapping each word to its frequency.
+     */
+    
+    /**
+     * Loads the text from a file
+     */
+    public void processLoad(String arg) throws Exception {
+        File f = new File(arg);
+        if (!f.exists()) {
+            throw new Exception("Missing or invalid argument.");
+        }
+        
+        System.out.printf("Loading text from '%s'\n", f.getAbsolutePath());
+        
+        /* TODO: Insert code for loading the text from the file into
+         * the internal map. */
+    }
+    
+    /**
+     * Prints the most frequent n words from the text.
+     */
+    public void processMost(int n) {
+        System.out.printf("Printing most frequent %d words:\n", n);
+        
+        /* TODO: Insert code for iterating through the map
+         * and print the most frequent n words.
+         */
+    }
+    
+    /**
+     * Prints the lest frequent n words from the text.
+     */
+    public void processLeast(int n) {
+        System.out.printf("Printing least frequent %d words:\n", n);
+
+        /* TODO: Insert code for iterating through the map
+         * and print the least frequent n words.
+         */
+    }
+    
+    /**
+     * Prints the frequency in the text of the given word.
+     */
+    public void processSearch(String word) {
+        System.out.printf("Searching frequency of word '%s'...\n", word);
+        
+        /* TODO: Insert code for looking up word in the map
+         * and print its frequency value.
+         */
+    }
+}
